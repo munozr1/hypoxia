e2ae3774fc308d29b80693b1bbecd95d0440c9a1
Rodrigo Munoz <rm117579@gmail.com>
2025-04-04 16:35:32 -0500

    Initial Commit

---
+++ Hypoxia/HypoxiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypoxiaApp.swift
+//  Hypoxia
+//
+//  Created by Rodrigo Munoz on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct HypoxiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
